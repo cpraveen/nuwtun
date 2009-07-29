6b5fae02a42d8b41f14e72a566ac50fc35b32486
Praveen. C <cpraveen@gmail.com>
2009-07-29 17:02:13 +0000

    Added driver for aoa sweep
    git-svn-id: https://svn.berlios.de/svnroot/repos/nuwtun/trunk@76 b4dca852-493c-0410-a6d7-ee0e29af0bb2

---
+++ src-utl/alpha_sweep.f
@@ -0,0 +1,60 @@
+C
+C     Driver routine to run flow solver for different ALPHA (AOA)
+C
+C     Specify min, max and increment for the AOA
+C     Cl and Cd will be written into alpha.dat
+C
+      program alpha_sweep
+      implicit none
+      character*64 flosolve, sdummy
+      real      Cd, Cl
+      integer   ifid, ifm, iter, i, imax
+      real      rdummy
+      real      alpha_min, alpha_max, dalpha, alpha
+
+      print*,'AOA: min, max, increment'
+      read*, alpha_min, alpha_max, dalpha
+
+      if(alpha_max.le.alpha_min .or. dalpha.le.0.0)then
+         print*,'Error: give alpha_max > alpha_min'
+         print*,'            dalpha > 0.0'
+         stop
+      endif
+
+      flosolve='$NUWTUN_HOME/src-flo/nuwtun_flo < flo.in > flo.log'
+      ifid    = 10
+      ifm     = 11
+
+c     Check file exists
+      open(ifid, file='flo.in.noalpha', status='old')
+      close(ifid)
+
+      imax    = (alpha_max - alpha_min)/dalpha
+      open(ifm, file="alpha.dat")
+
+      do i = 0, imax
+
+         alpha = alpha_min + dalpha * i
+
+         open(ifid, file='flo.in')
+         write(ifid,*)'ALPHA    ', alpha
+         close(ifid)
+
+         call system("cat flo.in.noalpha >> flo.in")
+         call system(flosolve)
+
+         open(ifid,file='fort.19', status='old')
+         read(ifid,*) sdummy, iter
+         read(ifid,*) sdummy, rdummy, rdummy, rdummy, rdummy, rdummy
+         read(ifid,*) sdummy, Cl
+         read(ifid,*) sdummy, Cd
+         close(ifid)
+         print*,'alpha, Cl, Cd =', alpha, Cl, Cd
+         write(ifm,*) alpha, Cl, Cd
+
+      enddo
+      close(ifm)
+
+      stop
+      end program
+
